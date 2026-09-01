7fb502d373ea9f9b091c162f476cd868eba83f01
Malinda <malindakpt@gmail.com>
2026-09-01 22:25:01 +0530

    Added sorting params for api

--- src/features/expenses/types/expense-api.types.ts
+++ src/features/expenses/types/expense-api.types.ts
@@ -1,10 +1,14 @@
 import type { PaginatedResponse } from "../../../types/paginatedResponse";
 import type { Expense, ExpenseCategory } from "./expense.types";
 
+export type ExpenseSortField = 'date' | 'amount';
+export type SortOrder = 'asc' | 'desc';
 export interface GetExpensesParams {
   page: number;
   limit: number;
   category?: ExpenseCategory;
+  sortBy: ExpenseSortField;
+  sortOrder: SortOrder;
 }
 
 export type PaginatedExpensesResponse = PaginatedResponse<Expense>;
